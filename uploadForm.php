4732c8543e43bb04275ebc728417b9eac1aae399
Tanuka Nayak <tn76@afsconnect1.njit.edu>
2017-10-14 16:41:17 -0400

    added comments in uploadForm.php

--- uploadForm.php
+++ uploadForm.php
@@ -1,30 +1,38 @@
 <?php
-        class uploadForm extends page
-        {
-            public function get()
-            {
-                $form = '<form action="index.php?page=uploadForm" method="POST" enctype="multipart/form-data">';
-                $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
-                $form .= '<input type="submit" value="Upload CSV" name="submit">';
-                $form .= '</form>';
-                $this->html .= htmlTags::headingOne('Upload Form');
-                $this->html .= $form;
-            }
-            public function post() {
-                    $target_dir = "uploads/";
-                    $target_file = $target_dir . $_FILES["fileToUpload"]["name"];
-                    $filename = $_FILES["fileToUpload"]["name"];
-
-                    if (file_exists($target_file)) {
-                        unlink($target_file);
-                    }
-                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                        //echo "The file " . $filename . " has been uploaded.";
-                        header("Location: index.php?page=htmlTable&filename=$filename");
-                    /*} else {
-                        echo "Sorry, there was an error uploading your file.";*/
-                    }
-            }
-
+    class uploadForm extends page
+    {
+        public function get() {                          //The get function displays the Upload Form and requests input from the user.
+            $form = '<form action="index.php?page=uploadForm" method="POST" enctype="multipart/form-data">';
+            $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
+            $form .= '<input type="submit" value="Upload CSV" name="submit">';
+            $form .= '</form>';
+            $this->html .= htmlTags::headingOne('Upload Form');
+            $this->html .= $form;
         }
+        public function post() {                         //The target directory is specified.
+            $target_dir = "uploads/";
+            $target_file = $target_dir . $_FILES["fileToUpload"]["name"];
+            $filename = $_FILES["fileToUpload"]["name"];
+
+        /*************If the uploaded file already exists, it will be deleted from the directory.**********/
+            if (file_exists($target_file)) {
+                unlink($target_file);
+            }
+
+	/***********If the file is saved in the 'uploads' directory successfully, the user is redirected to another page.************/
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                header("Location: index.php?page=htmlTable&filename=$filename");
+            }
+
+	/***********Alternatively, we can display to user that the file has been successfully uploaded,********/
+	/***********and then redirect to another path with a 5 second delay************************************/
+
+	/*  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                stringFunctions::printThis("The file " . $filename . "has been uploaded.");
+                header("Refresh: 5; url= index.php?page=htmlTable&filename=$filename");
+            } else {
+                stringFunctions::printThis("Sorry, there was an error uploading your file.");
+	    }    */
+	}
+    }
 ?>
