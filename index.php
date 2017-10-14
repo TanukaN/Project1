b27165a60a07002a3b3e687313c471f5e58bf1d5
Tanuka Nayak <tn76@afsconnect1.njit.edu>
2017-10-14 16:40:15 -0400

    added comments in index.php

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
 <?php
-	
-	//Debugging messages set to 'ON'
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);	
+    ini_set('display_errors', 'On');				//Debugging messages set to 'ON'
+    error_reporting(E_ALL);	
 
-	//Defining a class with autoload function to attempt to load undefined class
-	class Manage {
-		public static function autoload($class) {
-        		include $class . '.php';
-    		}
+    class Manage {						//Defining a class with autoload function to attempt to load undefined class
+	public static function autoload($class) {
+	    include $class . '.php';
 	}
-	spl_autoload_register(array('Manage', 'autoload'));
-
-	$obj = new main();	
-
-	class main {
-		public function __construct() {
-        		$pageRequest = 'uploadForm';
-        		if(isset($_REQUEST['page'])) {
-            			$pageRequest = $_REQUEST['page'];
-        		}
-        		$page = new $pageRequest;
-        		if($_SERVER['REQUEST_METHOD'] == 'GET') {
-            			$page->get();
-    			} else {
-						$page->post();
-				}
-		}
+    }
+    spl_autoload_register(array('Manage', 'autoload'));
+    
+    $obj = new main();						//Instantiating object for class main	
+    
+    class main {
+	public function __construct() {
+	    $pageRequest = 'uploadForm';
+	    if(isset($_REQUEST['page'])) {
+		$pageRequest = $_REQUEST['page'];
+	    }
+	    $page = new $pageRequest;
+	    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+		$page->get();
+	    } else {
+		$page->post();
+	    }
 	}
+    }
 ?>
