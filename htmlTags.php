6a38ff320102743b9de8aa9657ef7e8bd5614d4b
Tanuka Nayak <tn76@afsconnect1.njit.edu>
2017-10-14 16:43:05 -0400

    added tags

--- htmlTags.php
+++ htmlTags.php
@@ -1,7 +1,19 @@
 <?php
-  class htmlTags {
-    static public function headingOne($text) {
-      return '<h1>' . $text . '</h1>';
+    class htmlTags {				//html tags used in the project
+        static public function headingOne($text) {
+            return '<h1>' . $text . '</h1>';
+        }
+        static public function tableFormat() {
+            echo "<table cellpadding='5px' border='1px' style='border-collapse: collapse'>";
+        }
+        static public function tableHeader($text) {
+            echo '<th style="font-size: large">'.$text.'</th>';
+        }
+        static public function tableContent($text) {
+            echo '<td>'.$text.'</td>';
+        }
+        static public function breakTableRow() {
+            echo '</tr>';
+        }
     }
-  }
 ?>
